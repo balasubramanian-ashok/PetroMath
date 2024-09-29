2b00c6fed0fd72ba087f63de11483b8384294869
Ashok Balasubramanian <asbalasubramania@ebay.com>
2024-09-29 12:41:50 -0400

    DEV environment setup

--- config/app-deployment-dev.js
+++ config/app-deployment-dev.js
@@ -1,12 +1,12 @@
 module.exports = {
   // DB config
   //HOST: "ec2-35-174-200-24.compute-1.amazonaws.com",
-  HOST: "54.160.7.3",
+  HOST: "3.233.160.101",
   PORT: "3306",
   // USER: "dev_admin",   // Not working: failing with Access denied for user 'dev_admin'@'c-98-210-183-1.hsd1.ca.comcast.net' (using password: YES)
   USER: "root",
   PASSWORD: "welcome123",
-  DB: "petrolpump_dev",
+  DB: "petrolpump_dev1",
   DIALECT: "mysql",
   pool: {
     max: 1,
